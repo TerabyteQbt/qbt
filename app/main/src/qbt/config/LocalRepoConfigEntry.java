016e1dd1fb5e82ee07a68e965085c38d31f531e2
Keith Amling <keith.amling@gmail.com>
2015-10-11 18:13:36 -0700

    Extract LocalRepoAccessor

--- app/main/src/qbt/config/LocalRepoConfigEntry.java
+++ app/main/src/qbt/config/LocalRepoConfigEntry.java
@@ -2,19 +2,12 @@ package qbt.config;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import misc1.commons.Maybe;
-import misc1.commons.concurrent.treelock.ArrayTreeLock;
-import misc1.commons.concurrent.treelock.ArrayTreeLockPath;
-import misc1.commons.concurrent.treelock.SimpleTreeLockPath;
-import misc1.commons.concurrent.treelock.TreeLock;
-import misc1.commons.concurrent.treelock.TreeLockInterface;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import qbt.PackageDirectory;
 import qbt.PackageTip;
-import qbt.VcsTreeDigest;
 import qbt.VcsVersionDigest;
 import qbt.repo.CommonRepoAccessor;
+import qbt.repo.LocalRepoAccessor;
 import qbt.vcs.LocalVcs;
 
 public final class LocalRepoConfigEntry {
@@ -39,42 +32,7 @@ public final class LocalRepoConfigEntry {
             return null;
         }
         LOGGER.debug("Local repo check for " + repo + " at " + repoDir + " hit");
-        return new CommonRepoAccessor() {
-            private Path packageDir(String prefix) {
-                return prefix.isEmpty() ? repoDir : repoDir.resolve(prefix);
-            }
-
-            @Override
-            public PackageDirectory makePackageDirectory(final String prefix) {
-                lock(repo, prefix);
-                return new PackageDirectory() {
-                    @Override
-                    public Path getDir() {
-                        return packageDir(prefix);
-                    }
-
-                    @Override
-                    public void close() {
-                        unlock(repo, prefix);
-                    }
-                };
-            }
-
-            @Override
-            public VcsTreeDigest getEffectiveTree(Maybe<String> prefix) {
-                if(prefix.isPresent()) {
-                    return localVcs.getRepository(repoDir).getEffectiveTree(Paths.get(prefix.get(null)));
-                }
-                else {
-                    return localVcs.emptyTree();
-                }
-            }
-
-            @Override
-            public boolean isOverride() {
-                return true;
-            }
-        };
+        return new LocalRepoAccessor(localVcs, repoDir);
     }
 
     public RepoConfig.RequireRepoLocalResult findRepoLocal(final PackageTip repo) {
@@ -95,23 +53,5 @@ public final class LocalRepoConfigEntry {
                 return repoDir;
             }
         };
-    }
-
-    private static final class MyTreeLock extends TreeLock<PackageTip, ArrayTreeLockPath<String>> {
-        @Override
-        protected TreeLockInterface<? super ArrayTreeLockPath<String>> newChild() {
-            return new ArrayTreeLock<String>();
-        }
-    }
-    private final MyTreeLock inUse = new MyTreeLock();
-
-    private void lock(PackageTip repo, String prefix) {
-        LOGGER.debug("Local repo locking " + repo + "/" + prefix);
-        inUse.lock(SimpleTreeLockPath.of(repo, ArrayTreeLockPath.split(prefix, '/')));
-    }
-
-    private void unlock(PackageTip repo, String prefix) {
-        LOGGER.debug("Local repo unlocking " + repo + "/" + prefix);
-        inUse.unlock(SimpleTreeLockPath.of(repo, ArrayTreeLockPath.split(prefix, '/')));
     }
 }
---
+++ app/main/src/qbt/repo/LocalRepoAccessor.java
@@ -0,0 +1,72 @@
+package qbt.repo;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import misc1.commons.Maybe;
+import misc1.commons.concurrent.treelock.ArrayTreeLock;
+import misc1.commons.concurrent.treelock.ArrayTreeLockPath;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import qbt.PackageDirectory;
+import qbt.VcsTreeDigest;
+import qbt.repo.CommonRepoAccessor;
+import qbt.vcs.LocalVcs;
+
+public class LocalRepoAccessor implements CommonRepoAccessor {
+    private static final Logger LOGGER = LoggerFactory.getLogger(LocalRepoAccessor.class);
+
+    private final LocalVcs localVcs;
+    private final Path repoDir;
+
+    public LocalRepoAccessor(LocalVcs localVcs, Path repoDir) {
+        this.localVcs = localVcs;
+        this.repoDir = repoDir;
+    }
+
+    private Path packageDir(String prefix) {
+        return prefix.isEmpty() ? repoDir : repoDir.resolve(prefix);
+    }
+
+    @Override
+    public PackageDirectory makePackageDirectory(final String prefix) {
+        lock(prefix);
+        return new PackageDirectory() {
+            @Override
+            public Path getDir() {
+                return packageDir(prefix);
+            }
+
+            @Override
+            public void close() {
+                unlock(prefix);
+            }
+        };
+    }
+
+    @Override
+    public VcsTreeDigest getEffectiveTree(Maybe<String> prefix) {
+        if(prefix.isPresent()) {
+            return localVcs.getRepository(repoDir).getEffectiveTree(Paths.get(prefix.get(null)));
+        }
+        else {
+            return localVcs.emptyTree();
+        }
+    }
+
+    @Override
+    public boolean isOverride() {
+        return true;
+    }
+
+    private final ArrayTreeLock<String> inUse = new ArrayTreeLock<String>();
+
+    private void lock(String prefix) {
+        LOGGER.debug("Local repo locking " + repoDir.resolve(prefix));
+        inUse.lock(ArrayTreeLockPath.split(prefix, '/'));
+    }
+
+    private void unlock(String prefix) {
+        LOGGER.debug("Local repo unlocking " + repoDir.resolve(prefix));
+        inUse.unlock(ArrayTreeLockPath.split(prefix, '/'));
+    }
+}
