743ef7960f82596145196f3340ecb6d5f217abb5
Keith Amling <keith.amling@gmail.com>
2015-10-11 23:28:43 -0700

    Parallelize pushPins

--- app/main/src/qbt/mains/PushPins.java
+++ app/main/src/qbt/mains/PushPins.java
@@ -1,12 +1,17 @@
 package qbt.mains;
 
+import com.google.common.base.Function;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Iterables;
 import java.io.IOException;
 import java.util.Collection;
+import misc1.commons.concurrent.WorkPool;
+import misc1.commons.concurrent.ctree.ComputationTree;
+import misc1.commons.concurrent.ctree.ComputationTreeComputer;
 import misc1.commons.options.OptionsFragment;
 import misc1.commons.options.OptionsResults;
 import misc1.commons.options.UnparsedOptionsFragment;
+import org.apache.commons.lang3.ObjectUtils;
 import qbt.HelpTier;
 import qbt.PackageTip;
 import qbt.QbtCommand;
@@ -18,6 +23,7 @@ import qbt.VcsVersionDigest;
 import qbt.config.QbtConfig;
 import qbt.options.ConfigOptionsDelegate;
 import qbt.options.ManifestOptionsDelegate;
+import qbt.options.ParallelismOptionsDelegate;
 import qbt.options.RepoActionOptionsDelegate;
 import qbt.remote.QbtRemote;
 import qbt.repo.PinnedRepoAccessor;
@@ -28,6 +34,7 @@ public class PushPins extends QbtCommand<PushPins.Options> {
     public static interface Options extends QbtCommandOptions {
         public static final ConfigOptionsDelegate<Options> config = new ConfigOptionsDelegate<Options>();
         public static final ManifestOptionsDelegate<Options> manifest = new ManifestOptionsDelegate<Options>();
+        public static final ParallelismOptionsDelegate<Options> parallelism = new ParallelismOptionsDelegate<Options>();
         public static final RepoActionOptionsDelegate<Options> repos = new RepoActionOptionsDelegate<Options>(RepoActionOptionsDelegate.NoArgsBehaviour.THROW);
         public static final OptionsFragment<Options, ?, ImmutableList<String>> remote = new UnparsedOptionsFragment<Options>("QBT remote to which to push", false, 1, 1);
     }
@@ -49,21 +56,39 @@ public class PushPins extends QbtCommand<PushPins.Options> {
 
     @Override
     public int run(OptionsResults<? extends Options> options) throws IOException {
-        QbtConfig config = Options.config.getConfig(options);
-        QbtManifest manifest = Options.manifest.getResult(options).parse();
+        final QbtConfig config = Options.config.getConfig(options);
+        final QbtManifest manifest = Options.manifest.getResult(options).parse();
         Collection<PackageTip> repos = Options.repos.getRepos(config, manifest, options);
         String qbtRemoteString = Iterables.getOnlyElement(options.get(Options.remote));
-        QbtRemote qbtRemote = config.qbtRemoteFinder.requireQbtRemote(qbtRemoteString);
-        for(PackageTip repo : repos) {
-            RepoManifest repoManifest = manifest.repos.get(repo);
-            if(repoManifest == null) {
-                throw new IllegalArgumentException("No such repo [tip]: " + repo);
-            }
-            VcsVersionDigest version = repoManifest.version;
-            PinnedRepoAccessor pinnedAccessor = config.localPinsRepo.requirePin(repo, version);
-            RawRemote remote = qbtRemote.requireRemote(repo);
+        final QbtRemote qbtRemote = config.qbtRemoteFinder.requireQbtRemote(qbtRemoteString);
 
-            pinnedAccessor.pushToRemote(remote);
+        ComputationTree<?> computationTree = ComputationTree.transformIterable(repos, new Function<PackageTip, ObjectUtils.Null>() {
+            @Override
+            public ObjectUtils.Null apply(PackageTip repo) {
+                RepoManifest repoManifest = manifest.repos.get(repo);
+                if(repoManifest == null) {
+                    throw new IllegalArgumentException("No such repo [tip]: " + repo);
+                }
+                VcsVersionDigest version = repoManifest.version;
+                PinnedRepoAccessor pinnedAccessor = config.localPinsRepo.requirePin(repo, version);
+                RawRemote remote = qbtRemote.requireRemote(repo);
+
+                pinnedAccessor.pushToRemote(remote);
+
+                return ObjectUtils.NULL;
+            }
+        });
+        final WorkPool workPool = Options.parallelism.getResult(options, false).createWorkPool();
+        try {
+            new ComputationTreeComputer() {
+                @Override
+                protected void submit(Runnable r) {
+                    workPool.submit(r);
+                }
+            }.await(computationTree).getCommute();
+        }
+        finally {
+            workPool.shutdown();
         }
         return 0;
     }
