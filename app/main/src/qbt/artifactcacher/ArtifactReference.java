499516fb7d9d4435d0bfb5907b745cd5d4b93965
Keith Amling <keith.amling@gmail.com>
2016-01-03 20:16:27 -0800

    JDK8: ResourceType

--- app/main/src/qbt/artifactcacher/ArtifactReference.java
+++ app/main/src/qbt/artifactcacher/ArtifactReference.java
@@ -1,6 +1,5 @@
 package qbt.artifactcacher;
 
-import com.google.common.base.Function;
 import java.nio.file.Path;
 import misc1.commons.Maybe;
 import misc1.commons.resources.FreeScope;
@@ -8,23 +7,19 @@ import misc1.commons.resources.Resource;
 import misc1.commons.resources.ResourceType;
 
 public interface ArtifactReference extends BaseArtifactReference, Resource<ArtifactReference> {
-    public static final ResourceType<RawArtifactReference, ArtifactReference> TYPE = new ResourceType<RawArtifactReference, ArtifactReference>() {
-        public ArtifactReference wrap(final RawArtifactReference raw, final Function<FreeScope, ArtifactReference> onCopyInto) {
-            return new ArtifactReference() {
-                @Override
-                public void materializeDirectory(Maybe<FreeScope> scope, Path destination) {
-                    raw.materializeDirectory(scope, destination);
-                }
+    public static final ResourceType<RawArtifactReference, ArtifactReference> TYPE = (raw, onCopyInto) -> new ArtifactReference() {
+        @Override
+        public void materializeDirectory(Maybe<FreeScope> scope, Path destination) {
+            raw.materializeDirectory(scope, destination);
+        }
 
-                @Override
-                public void materializeTarball(Path destination) {
-                    raw.materializeTarball(destination);
-                }
+        @Override
+        public void materializeTarball(Path destination) {
+            raw.materializeTarball(destination);
+        }
 
-                public ArtifactReference copyInto(FreeScope scope) {
-                    return onCopyInto.apply(scope);
-                }
-            };
+        public ArtifactReference copyInto(FreeScope scope) {
+            return onCopyInto.apply(scope);
         }
     };
 }
