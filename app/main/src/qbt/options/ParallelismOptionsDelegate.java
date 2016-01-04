48a333840a0e40066d0833f96e9fc5c0e69cff12
Keith Amling <keith.amling@gmail.com>
2016-01-03 20:36:18 -0800

    JDK8: ParallelismOptionsResult

--- app/main/src/qbt/options/ParallelismOptionsDelegate.java
+++ app/main/src/qbt/options/ParallelismOptionsDelegate.java
@@ -3,8 +3,6 @@ package qbt.options;
 import com.google.common.collect.ImmutableList;
 import misc1.commons.Maybe;
 import misc1.commons.concurrent.WorkPool;
-import misc1.commons.concurrent.ctree.ComputationTree;
-import misc1.commons.concurrent.ctree.ComputationTreeComputer;
 import misc1.commons.options.NamedBooleanFlagOptionsFragment;
 import misc1.commons.options.NamedIntegerSingletonArgumentOptionsFragment;
 import misc1.commons.options.OptionsDelegate;
@@ -15,53 +13,17 @@ public class ParallelismOptionsDelegate<O> implements OptionsDelegate<O> {
     public final OptionsFragment<O, ?, Integer> parallelism = new NamedIntegerSingletonArgumentOptionsFragment<O>(ImmutableList.of("--parallelism", "-j"), Maybe.<Integer>of(null), "Parallelize up to this width");
     public final OptionsFragment<O, ?, Boolean> infiniteParallelism = new NamedBooleanFlagOptionsFragment<O>(ImmutableList.of("--infinite-parallelism", "-J"), "Parallelize as widely as possible");
 
-    private static abstract class BaseParallelismOptionsResult implements ParallelismOptionsResult {
-        @Override
-        public <T> T runComputationTree(ComputationTree<T> computationTree) {
-            try(WorkPool workPool = createWorkPool()) {
-                return new ComputationTreeComputer(workPool).await(computationTree).getCommute();
-            }
-        }
-    }
-
-    private static class ExplicitParallelismOptionsResult extends BaseParallelismOptionsResult {
-        private final int parallelism;
-
-        public ExplicitParallelismOptionsResult(int parallelism) {
-            this.parallelism = parallelism;
-        }
-
-        @Override
-        public WorkPool createWorkPool() {
-            return WorkPool.explicitParallelism(parallelism);
-        }
-    }
-
-    private static class InfiniteParallelismOptionsResult extends BaseParallelismOptionsResult {
-        @Override
-        public WorkPool createWorkPool() {
-            return WorkPool.infiniteParallelism();
-        }
-    }
-
-    private static class DefaultParallelismOptionsResult extends BaseParallelismOptionsResult {
-        @Override
-        public WorkPool createWorkPool() {
-            return WorkPool.defaultParallelism();
-        }
-    }
-
     public ParallelismOptionsResult getResult(OptionsResults<? extends O> options, boolean isInteractive) {
         if(isInteractive) {
-            return new ExplicitParallelismOptionsResult(1);
+            return () -> WorkPool.explicitParallelism(1);
         }
         if(options.get(infiniteParallelism)) {
-            return new InfiniteParallelismOptionsResult();
+            return () -> WorkPool.infiniteParallelism();
         }
         Integer explicitParallelism = options.get(parallelism);
         if(explicitParallelism != null) {
-            return new ExplicitParallelismOptionsResult(explicitParallelism);
+            return () -> WorkPool.explicitParallelism(explicitParallelism);
         }
-        return new DefaultParallelismOptionsResult();
+        return () -> WorkPool.defaultParallelism();
     }
 }
--- app/main/src/qbt/options/ParallelismOptionsResult.java
+++ app/main/src/qbt/options/ParallelismOptionsResult.java
@@ -2,8 +2,13 @@ package qbt.options;
 
 import misc1.commons.concurrent.WorkPool;
 import misc1.commons.concurrent.ctree.ComputationTree;
+import misc1.commons.concurrent.ctree.ComputationTreeComputer;
 
 public interface ParallelismOptionsResult {
     WorkPool createWorkPool();
-    <T> T runComputationTree(ComputationTree<T> computationTree);
+    default <T> T runComputationTree(ComputationTree<T> computationTree) {
+        try(WorkPool workPool = createWorkPool()) {
+            return new ComputationTreeComputer(workPool).await(computationTree).getCommute();
+        }
+    }
 }
