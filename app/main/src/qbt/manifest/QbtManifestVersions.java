43e3893f99f94b858296498cec6fa4119cbbcece
Keith Amling <keith.amling@gmail.com>
2017-05-15 19:58:04 -0700

    Destroy V0-V2.  After this there is no going back.

--- app/main/src/qbt/manifest/QbtManifestVersions.java
+++ app/main/src/qbt/manifest/QbtManifestVersions.java
@@ -1,63 +1,28 @@
 package qbt.manifest;
 
-import com.google.common.collect.ImmutableList;
 import java.util.List;
 import qbt.manifest.current.QbtManifest;
 
 public class QbtManifestVersions {
-    private static class Internals {
-        public final ImmutableList<QbtManifestVersion<?, ?>> list;
-
-        public Internals(ImmutableList<QbtManifestVersion<?, ?>> list) {
-            this.list = list;
-
-            for(int i = 0; i < list.size(); ++i) {
-                if(list.get(i).version != i) {
-                    throw new IllegalArgumentException();
-                }
-                if(i + 1 < list.size()) {
-                    QbtManifestVersion<?, ?> before = list.get(i);
-                    QbtManifestVersion<?, ?> after = list.get(i + 1);
-                    if(!(before instanceof QbtManifestUpgradeableVersion)) {
-                        throw new IllegalArgumentException();
-                    }
-                    if(!((QbtManifestUpgradeableVersion<?, ?, ?>) before).upgradeManifestClass.equals(after.manifestClass)) {
-                        throw new IllegalArgumentException();
-                    }
-                }
-            }
-        }
-    }
-
-    static final V0QbtManifestVersion V0;
-    static final V1QbtManifestVersion V1;
-    static final V2QbtManifestVersion V2;
-    static final V3QbtManifestVersion V3;
-    private static final Internals INTERNALS;
-    static {
-        ImmutableList.Builder<QbtManifestVersion<?, ?>> b = ImmutableList.builder();
-
-        b.add(V3 = new V3QbtManifestVersion());
-        b.add(V2 = new V2QbtManifestVersion(V3));
-        b.add(V1 = new V1QbtManifestVersion(V2));
-        b.add(V0 = new V0QbtManifestVersion(V1));
-
-        INTERNALS = new Internals(b.build().reverse());
-    }
+    static final V3QbtManifestVersion V3 = new V3QbtManifestVersion();
 
     public static LegacyQbtManifest<?, ?> parseLegacy(List<String> lines) {
         final QbtManifestVersion<?, ?> version;
         if(lines.isEmpty()) {
-            version = V0;
+            throw new IllegalArgumentException();
         }
         else {
             String line0 = lines.get(0);
             if(!line0.startsWith("@")) {
-                version = V0;
+                throw new IllegalArgumentException();
             }
             else {
                 lines = lines.subList(1, lines.size());
-                version = INTERNALS.list.get(Integer.parseInt(line0.substring(1), 10));
+                int n = Integer.parseInt(line0.substring(1), 10);
+                if(n != 3) {
+                    throw new IllegalArgumentException();
+                }
+                version = V3;
             }
         }
         final List<String> linesFinal = lines;
