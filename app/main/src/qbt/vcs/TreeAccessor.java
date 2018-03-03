22f33ab439c49a73921496dc31efcb5f73852b16
Keith Amling <amling@palantir.com>
2018-03-03 14:12:06 -0800

    TreeAccessor lines functions.

--- app/main/src/qbt/vcs/TreeAccessor.java
+++ app/main/src/qbt/vcs/TreeAccessor.java
@@ -2,9 +2,22 @@ package qbt.vcs;
 
 import java.util.Collection;
 import misc1.commons.Either;
+import qbt.QbtUtils;
 import qbt.VcsTreeDigest;
 
 public interface TreeAccessor {
+    public default Iterable<String> requireFileLines(String path) {
+        Either<TreeAccessor, byte[]> e = get(path);
+        if(e == null || e.isLeft()) {
+            throw new IllegalArgumentException("No " + path);
+        }
+        return QbtUtils.bytesToLines(e.rightOrNull());
+    }
+
+    public default TreeAccessor replace(String path, Iterable<String> lines) {
+        return replace(path, QbtUtils.linesToBytes(lines));
+    }
+
     public default TreeAccessor replace(String path, byte[] contents) {
         return replace(path, Either.right(contents));
     }
